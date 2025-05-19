751d2b74e373730a55a4dddbf160bf68091d183d
coderpy04 <ritesh.2201141ec@iiitbh.ac.in>
2025-05-19 10:37:47 +0530

    final

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const {connectToMongoDB} = require('./connect');
 const urlRoute = require('./routes/url')
 
 const app = express();
 const PORT = 8001;
 
+connectToMongoDB('mongodb://localhost:').then(() => {console.log('connected to mongodb')});
+
+app.use(express.json());      
 app.use('/url',urlRoute);
 
 app.listen(PORT, () => console.log(`Server has started at PORT:{PORT}`))
